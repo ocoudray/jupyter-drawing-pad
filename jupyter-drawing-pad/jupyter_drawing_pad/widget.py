f228a5f0e0ea61d6a1a198bfa7f52126fe05bb0a
Olivier Coudray <olivier.coudray@polytechnique.edu>
2017-10-25 15:10:54 +0200

    button save + login (not working!)

--- jupyter-drawing-pad/jupyter_drawing_pad/widget.py
+++ jupyter-drawing-pad/jupyter_drawing_pad/widget.py
@@ -1,12 +1,27 @@
 from .example import DrawingPad
-from ipywidgets import HBox, Button
+from ipywidgets import HBox, VBox, Button
 from IPython import display
 
 class CustomBox(HBox):
     def __init__(self):
         drawing_pad = DrawingPad()
         button = Button(description="Clear", tooltip="Click me")
+        save_button = Button(description="Save", tooltip="Click me")
+        login_button = Button(description="Login", tooltip="Click me")
         button.on_click(lambda b : drawing_pad.clear())
+        save_button.on_click(lambda b : self.set_saved())
+        login_button.on_click(lambda b : self.check())
+        buttons = VBox([button, save_button, login_button])
         self.drawing_pad = drawing_pad
-        super().__init__([drawing_pad, button])
+        self.__saved = []
+        super().__init__([drawing_pad, buttons])
+
+    def set_saved(self):
+        self.__saved = self.drawing_pad.data
+    
+    def get_saved(self):
+        return self.__saved
+    
+    def check(self):
+        print("ok")
     
